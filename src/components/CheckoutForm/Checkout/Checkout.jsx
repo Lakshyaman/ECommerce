99fb2840af18464b9c016600978a808b0a96ce3c
Lakshya <manlakshya28@gmail.com>
2021-01-02 21:23:13 +0530

    final work

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -5,6 +5,7 @@ import { commerce } from '../../../lib/commerce';
 import useStyles from './styles'
 import AddressForm  from '../AddressForm';
 import PaymentForm from '../PaymentForm';
+import { Link } from 'react-router-dom';
 
 const steps = ['Shipping address', 'Payment details'];
 
@@ -22,7 +23,7 @@ const Checkout = ({cart, order, onCaptureCheckout, error}) => {
                 setCheckoutToken(token);
 
             } catch(error) {
-
+                console.log(error)
             }
         }
 
@@ -38,11 +39,31 @@ const Checkout = ({cart, order, onCaptureCheckout, error}) => {
         nextStep();
     }
 
-    const Confirmation = () => (
-        <div>
-            ConfirmationForm
+    let Confirmation = () => order.customer ? (
+        <>
+            <div>
+                <Typography variant="h5">Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}</Typography>
+                <Divider className={classes.divider} />
+                <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
+            </div>
+            <br />
+            <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
+        </>
+    ) :
+    (
+        <div className={classes.spinner}>
+            <CircularProgress />
         </div>
-    )
+    );
+
+    if(error) {
+        <>
+        <Typography variant="h5">Error: {error}</Typography>
+        <br />
+        <Button component={Link} to="/" variant="outlined" type="button">Back to Home</Button>
+        </>
+    }
+
     const Form = () => activeStep === 0
         ? <AddressForm checkoutToken={checkoutToken} next={next}/>
         : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep} onCaptureCheckout={onCaptureCheckout} nextStep={nextStep}/>
